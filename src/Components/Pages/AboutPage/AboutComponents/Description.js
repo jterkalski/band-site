a047986a9037e6c70a8061ada03fb76f62521515
Jakub <jakub.terkalski@gmail.com>
2022-04-01 19:33:42 +0200

    fixed scaling bug

--- src/Components/Pages/AboutPage/AboutComponents/Description.js
+++ src/Components/Pages/AboutPage/AboutComponents/Description.js
@@ -2,7 +2,13 @@ import React from 'react';
 import styles from '../About.module.scss';
 
 const Description = () => {
-    return <div className={styles.description}>Description</div>;
+    return (
+        <div className={styles.description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam feugiat dignissim tincidunt. Aenean id
+            gravida nisi, eu mollis magna. Donec blandit metus et velit feugiat vestibulum. Sed ultricies neque nunc,
+            sed vehicula massa sodales quis. Aliquam lobortis convallis turpis, ac ultricies justo condimentum nec.
+        </div>
+    );
 };
 
 export default Description;
--- src/Components/Pages/AboutPage/AboutComponents/Info.js
+++ src/Components/Pages/AboutPage/AboutComponents/Info.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import styles from '../About.module.scss';
+import { IoLocationSharp } from 'react-icons/io5';
+import { MdEmail } from 'react-icons/md';
 
 const Info = () => {
     return (
         <div className={styles.info}>
-            <div className={styles.infoItem}>Location: Kraków, Poland</div>
-            <div className={styles.infoItem}>Contact: bepis.boys.01@gmail.com</div>
+            <div className={styles.infoItem}>
+                <div className={styles.infoHeader}>
+                    <IoLocationSharp className={styles.infoIcon} size={70} />
+                    <div>Location</div>
+                </div>
+                <div className={styles.infoDetails}>Kraków, Poland</div>
+            </div>
+            <div className={styles.infoItem}>
+                <div className={styles.infoHeader}>
+                    <MdEmail className={styles.infoIcon} size={70} />
+                    <div>Contact</div>
+                </div>
+                <div className={styles.infoDetails}>bepis.boys.01@gmail.com</div>
+            </div>
         </div>
     );
 };
